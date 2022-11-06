c442e5d207af8d8ea96ded8b07e901ba41266d4f
surya.madhav <saisurya1603@gmail.com>
2022-11-06 14:02:21 +0530

    basic

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,46 @@
-import { banner } from "./introbanner";
-import { Data } from './data.interface';
-// import { addCheckBox, addText } from "./inner-html.helper";
 var fs = require('fs');
-const puppeteer = require('puppeteer')
+import * as puppeteer from "puppeteer";
 let args = process.argv
-let data = JSON.parse(args[3]);
-// console.log(banner);
-// let data: Data = {
-//     "mode-of-transaction-cash": { type: "checkbox", value: true },
-//     "first-name": { type: "text", value: "Rebbapragada" },
-//     "middle-name": { type: "text", value: "Sai Surya" },
-//     "last-name": { type: "text", value: "Madhav" },
-//     "pin-code": { type: "text", value: "575030" },
-//     "mobile-number": { type: "text", value: "9686397040" },
-//     "mode-of-transaction-cheque": { type: "checkbox", value: false },
-//     "mode-of-transaction-card": { type: "checkbox", value: true }
-// }
-async function generatePdf(data:any, template:string) {
+console.log("---- Reading definitions from vocabulary.com -----");
+
+(async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    const html = fs.readFileSync(`./src/html-templates/${template}.html`, 'utf-8');
-    page.on('console', async (msg: any) => {
-        const msgArgs = msg.args();
-        for (let i = 0; i < msgArgs.length; ++i) {
-            console.log(await msgArgs[i].jsonValue());
-        }
-    });
-    await page.setContent(html, { waitUntil: 'domcontentloaded' });
-    await page.emulateMediaType('print');
-    await page.waitForSelector("#page-container");
-    await page.evaluate((data: Data) => {
-        function addText(element:HTMLElement,value:string){
-            element.innerHTML = value;
-        }
-        function addCheckBox(element: HTMLElement,value:boolean){
-            element.style.opacity = value?"100%":"0";
-        }
-        let dom = document.querySelectorAll('.kaldata');
-        for (let index = 0; index < dom.length; index++) {
-            const element = dom[index];
-            if (document.querySelector("#" + element.id)) {
-                let qs = document.querySelector("#" + element.id) as HTMLElement;
-                const d = data[element.id];
-                if(d.type=="text"){
-                    addText(qs ,d.value  as string);
-                }else if(data[element.id].type=="checkbox"){
-                    addCheckBox(qs,d.value as boolean);
-                }
-            }
-        }
-    }, data);
-    const pdf = await page.pdf({
-        path: 'result.pdf',
-        margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
-        printBackground: true,
-        format: 'A4',
-    });
+    let words = ["befuddleds", "acumen"];
+    let result = [];
+    for (let index = 0; index < words.length; index++) {
+        const element = words[index];
+        const res = await getWordData(element);
+        result.push(res);
+    }
     await browser.close();
-    // return pdf;
-    return "Generated PDF"
-}
+    console.log(result);
 
-generatePdf(data, "form-60").then(res => { console.log(res), (err: any) => { console.error(err) } });
+    async function getWordData(word) {
+        await page.goto(`https://www.vocabulary.com/dictionary/${word}`);
+        await page.waitForSelector(".definitionsContainer");
+        const res = await page.evaluate(() => {
+            // Find word
+            let word = document.querySelector("#hdr-word-area").textContent;
+            word = word.slice(word.indexOf("\n\t\t\t\t\t") + 6, word.indexOf("\n\t\t\t\t\t\n\t\t\t\t\t"));
+            let shortDef = document.querySelector(".short").textContent;
+            let longDef = document.querySelector(".long").textContent;
+            let definitions = [];
+            let def_nodes = document.querySelector(".definitionsContainer").querySelector("ol").querySelectorAll("li");
+            def_nodes.forEach(node => {
+                const grammer_node = node.querySelector(".pos-icon");
+                const grammer = node.querySelector(".pos-icon").textContent;
+                let def = node.innerText;
+                definitions.push({ grammer, def });
+
+            });
+            return {
+                "word": word,
+                "shortDef": shortDef,
+                "longDef": longDef,
+                "definitions": definitions
+            };
+        });
+        return res;
+    }
+})()
